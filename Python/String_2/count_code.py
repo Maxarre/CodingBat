95df6b924e96bd316efcd52233eb7d6d542a6349
Maxence Poncelet <maxenceponcelet@hotmail.com>
2020-07-31 23:26:55 +0200

    count_code.py

---
+++ Python/String_2/count_code.py
@@ -0,0 +1,16 @@
+# Return the number of times that the string "code" appears 
+# anywhere in the given string, except we'll accept any letter 
+# for the 'd', so "cope" and "cooe" count.
+
+
+# count_code('aaacodebbb') → 1
+# count_code('codexxcode') → 2
+# count_code('cozexxcope') → 2
+
+def count_code(str):
+  count = 0 
+  words = ['co'+d+'e' for d in 'abcdefghijklmnopqrstuvwxyz']
+  for word in words: 
+    n = str.count(word)
+    count += n
+  return count
